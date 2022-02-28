5c2199b2805f9284a7b8d7ddaecc7df79d37496d
Speedrun <86620608+Bayu12345677@users.noreply.github.com>
2022-03-01 00:52:08 +0700

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# author : polygon
+
+. lib/moduler.sh
+
+# command update
+
+rm -rf ../SpamOtp
+git clone https://github.com/Bayu12345677/SpamOtp
+
+Bash.import: util/IO.SYSTEM.log
+echo
+println_info " Script berhasil di update"
+println_info " tolong jalankan command (cd .) untuk mengupdate source nya"; echo
